4597472c3c39f50f166bf78fb3e8915516fe0497
arseni-baranov <arseni-baranov@yandex.ru>
2016-10-20 07:06:46 +0500

    Скрипт переписан с нуля, с учётом всех замечаний
    Множество мелких функций проверок вшиты в одну общую функцию, которая условно делится на проверки по символам, и по формату (дата, телефон и прочие). Некоторый функционал был выброшен, такой как проверка по имени и работе, так как я предпочел затрагивать настолько личные данные при проверке пароля. Остальной функционал формата весь переписан через регулярные выражения. Добавлен красивый вывод шкалы сложности пароля. Код стал полностью PEP 8, намного аккуратнее и чище чем в прошлый раз.

--- password_strength.py
+++ password_strength.py
@@ -1,153 +1,100 @@
 import re
-import os.path
 
 
-def check_email(user_email):
+def run_password_checks(pwd, blacklist):
 
-    email_words = user_email.split('@')
+    def check_initial_strength():
+        if len(pwd) < 5:
+            initial_strength = 1
+        elif len(pwd) <= 8:
+            initial_strength = 2
+        else:
+            initial_strength = 3
 
-    if user_email not in password:
-        for word in email_words:
-            if word not in password:
-                return True
+        return initial_strength
 
-
-def check_cellphone(cellphone):
-
-    if cellphone not in password:
-        return True
-
-
-def check_cellphone_format(user_cell):
-
-    if user_cell[0:1] == '+':
-        user_cell = user_cell[2:]
-        return user_cell
-    else:
-        return user_cell
-
-
-def birthday_split_check(delimiter, user_bday):
-    bday_list = user_bday.split(delimiter)
-
-    for bday in bday_list:
-        if bday not in password and user_bday not in password:
+    def pwd_case_check():
+        if pwd != pwd.lower() and pwd != pwd.upper():
             return True
+        else:
+            return False
 
+    def pwd_numerical_check():
+        numbers = re.findall(r'\d+', pwd)
+        letters = re.findall(r'[A-Za-z]', pwd)
 
-def check_birthsday(user_bday):
+        if len(numbers) > 0 and len(letters) > 0:
+            return True
+        else:
+            return False
 
-    if '.' in user_bday:
-        return birthday_split_check('.', user_bday)
+    def pwd_spchar_check():
+        special_char = re.findall(r'[^A-Za-z0-9]', pwd)
 
-    elif '/' in user_bday:
-        return birthday_split_check('/', user_bday)
+        if len(special_char) > 0:
+            return True
+        else:
+            return False
 
-    elif ' ' in user_bday:
-        return birthday_split_check(' ', user_bday)
+    def pwd_blacklist_check(blacklist):
+        with open(blacklist) as blacklist:
+            blacklist = blacklist.read().splitlines()
 
+        if pwd not in blacklist:
+            return True
+        else:
+            return False
 
-def check_user_name(password, user_name):
+    checklist = [check_initial_strength(),
+                 pwd_case_check(),
+                 pwd_numerical_check(),
+                 pwd_spchar_check(),
+                 pwd_blacklist_check(blacklist)]
 
-    username_strength = 0
+    strength = sum(checklist)
 
-    for name in user_name:
-        if name not in password:
-            username_strength += 1
+    def personal_info_check():
 
-    return username_strength
+        date_check = re.compile(r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$')
+        license_check = re.compile(r'[а-я]\d{3}[а-я]{2}\d{2,3}')
+        email_check = re.compile(r'^.+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?)$')
+        cell_phone = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
 
+        def pwd_check_formats(format):
+            result = re.search(format, pwd)
+            if result is not None:
+                return True
+            else:
+                return False
 
-def check_user_data(password):
+        format_checklist = [pwd_check_formats(date_check),
+                            pwd_check_formats(license_check),
+                            pwd_check_formats(email_check),
+                            pwd_check_formats(cell_phone)]
 
-    add_to_strength = 0
+        if sum(format_checklist) == 0:
+            # Пароль надёжный, проверка пройдена
+            return True
+        else:
+            return False
 
-    birthsday = input('Введите вашу дату рождения: ')
-    user_name = input('Введите ваше ФИО: ').split(' ')
-    cellphone = check_cellphone_format(input('Введите ваш номер телефона: '))
-    user_mail = input('Введите ваш email: ')
-    user_job = input('Введите ваше место работы: ')
-    user_license_plate = input('Введите номер вашей машины (при наличии): ')
+    if personal_info_check():
+        strength += 3
 
-    if check_user_name(password, user_name) is 3:
-        add_to_strength += 1
-
-    if user_job not in password:
-        if user_license_plate not in password:
-            if check_email(user_mail) is True:
-                add_to_strength += 1
-
-    if check_birthsday(birthsday) is True:
-        if check_email(user_mail) is True:
-            if check_cellphone(cellphone) is True:
-                add_to_strength += 1
-
-    return add_to_strength
-
-
-def password_blacklist_check(blacklist_file):
-
-    with open(blacklist_file) as blacklist:
-        password_list = blacklist.read().splitlines()
-
-    if password not in password_list:
-        return True
-
-
-def password_spec_char_check():
-    special_char = re.findall(r'[^A-Za-z0-9]', password)
-
-    if len(special_char) > 0:
-        return True
-
-
-def password_numbers_check():
-    numbers = re.findall(r'\d+', password)
-    letters = re.findall(r'[A-Za-z]', password)
-
-    if len(numbers) > 0 and len(letters) > 0:
-        return True
-
-
-def password_case_check():
-    pass_lower = password.lower()
-    pass_upper = password.upper()
-
-    if not (pass_lower == password or pass_upper == password):
-        return True
-
-
-def get_password_strength(blacklist):
-    if len(password) < 5:
-        pass_strength = 1
-    elif len(password) <= 8:
-        pass_strength = 2
-    else:
-        pass_strength = 3
-
-    if password_case_check() is True:
-        pass_strength += 1
-
-    if password_numbers_check() is True:
-        pass_strength += 1
-
-    if password_spec_char_check() is True:
-        pass_strength += 1
-
-    if password_blacklist_check(blacklist) is True:
-        pass_strength += 1
-
-    return 'Сложность вашего пароля: ' + str(pass_strength + check_user_data(password)) + '/10'
-
+    return strength
 
 if __name__ == '__main__':
 
-    blacklist = input('Введие название текстового файла с часто используемыми паролями: ')
+    pwd = input('Введите пароль для оценки его сложности: ')
+    blacklist = input('Введите текстовый файл с чёрным списком паролей: ')
 
-    current_dir = os.path.abspath(__file__)
-    script_name = os.path.basename(__file__)
+    # Результат
+    
+    print('Сложность вашего пароля: ', run_password_checks(pwd, blacklist), '/ 10')
 
-    blacklist_file = current_dir.replace(script_name, '') + blacklist
+    # Красивый вывод
+    
+    strength_string = '█' * run_password_checks(pwd, blacklist)
+    empty_strength = '░' * (10 - run_password_checks(pwd, blacklist))
 
-    password = input('Введите пароль, и мы оценим его сложность : ')
-    print(get_password_strength(blacklist_file))
+    print(strength_string + empty_strength)
